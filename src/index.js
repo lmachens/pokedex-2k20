fffcfa2d291cfb70ec9b8c4750440ba2531465ca
lmachens <leon.machens@googlemail.com>
2020-03-02 15:59:41 +0100

    Add SASS loader

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
+import './index.scss';
+
 import { pokemon } from './components/pokemon';
 
 document.body.appendChild(pokemon());
--- webpack.config.js
+++ webpack.config.js
@@ -16,5 +16,20 @@ module.exports = {
   ],
   devServer: {
     contentBase: './dist'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          // Creates `style` nodes from JS strings
+          'style-loader',
+          // Translates CSS into CommonJS
+          'css-loader',
+          // Compiles Sass to CSS
+          'sass-loader'
+        ]
+      }
+    ]
   }
 };
