37fd8eabfd8b7b658a9b3c4bb4c21e1b3804c94e
lmachens <leon.machens@googlemail.com>
2020-03-02 17:36:22 +0100

    Add search component

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import './index.scss';
 
 import { title } from './components/title';
+import { search } from './components/search';
 
 document.body.appendChild(title('Pokedex 2k20'));
+document.body.appendChild(search());
