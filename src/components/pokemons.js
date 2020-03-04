626c37b8add99bde88abf3b22662e405a8ba2504
lmachens <leon.machens@googlemail.com>
2020-03-04 16:41:54 +0100

    Add favorite pokemon to localStorage

--- src/components/pokemons.js
+++ src/components/pokemons.js
@@ -11,6 +11,11 @@ export function createSearchResults(props) {
       innerText: item,
       className: 'pokemon'
     });
+    element.addEventListener('click', () => {
+      const favorites = [item];
+      const favoritesJSON = JSON.stringify(favorites);
+      localStorage.setItem('favorites', favoritesJSON);
+    });
     appendContent(container, element);
   });
   return container;
